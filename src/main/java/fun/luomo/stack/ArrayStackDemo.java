13f0413fb13bbbe15c80e64707c1c8a1ccea4471
Luomo <guoruiqiang_2018@163.com>
2020-07-01 16:28:24 +0800

    数组栈

---
+++ src/main/java/fun/luomo/stack/ArrayStackDemo.java
@@ -0,0 +1,21 @@
+package fun.luomo.stack;
+
+/**
+ * @author Luomo
+ * create 2020/7/1 16:22
+ */
+public class ArrayStackDemo {
+
+
+    public static void main(String[] args) {
+    }
+}
+
+
+class ArrayStack {
+
+    public void push(int v) {
+
+    }
+
+}
