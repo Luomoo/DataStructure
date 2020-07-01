c49ddfa10064d8c10198146812f3c0a25e0f1a80
Luomo <guoruiqiang_2018@163.com>
2020-07-01 22:20:03 +0800

    栈

--- src/main/java/fun/luomo/stack/ArrayStackDemo.java
+++ src/main/java/fun/luomo/stack/ArrayStackDemo.java
@@ -8,14 +8,65 @@ public class ArrayStackDemo {
 
 
     public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack(5);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.list();
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
     }
 }
 
 
 class ArrayStack {
+    private int maxSize;
+    private int[] stack;
+    private int top = -1;
 
-    public void push(int v) {
+    public ArrayStack(int maxSize) {
+        this.maxSize = maxSize;
+        stack = new int[this.maxSize];
+    }
 
+    public boolean isFull() {
+        return top == maxSize - 1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public void push(int value) {
+        if (isFull()) {
+            return;
+        }
+        top++;
+        stack[top] = value;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("空");
+        }
+        return stack[top--];
+    }
+
+    public void list() {
+        if(isEmpty()){
+            throw new RuntimeException("空");
+        }
+        for (int i=top;i>=0;i--){
+            System.out.println(stack[i]);
+
+        }
     }
 
 }
