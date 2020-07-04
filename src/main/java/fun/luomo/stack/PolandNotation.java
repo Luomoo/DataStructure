0e51904ec20ffac15882ac4bbfcd1eed01776717
Luomo <guoruiqiang_2018@163.com>
2020-07-04 23:28:39 +0800

    计算器 加入注释

--- src/main/java/fun/luomo/stack/PolandNotation.java
+++ src/main/java/fun/luomo/stack/PolandNotation.java
@@ -17,28 +17,46 @@ public class PolandNotation {
 //        String suffixExpression = "4 5 * 8 - 60 + 8 2 / +";
 //        List<String> listString = getListString(suffixExpression);
 //        System.out.println("结果 = " + calculate(listString));
-
+        //中缀表达式
         String Expression = "1+((2+3)*4)-5";// 1 2 3 + 4 * 5 -
+        //转成后缀表达式
         String s = toSuffixExpressionList(Expression);
         System.out.println(s);
+        //把后缀表达式转成list集合
         List<String> listString = getListString(s);
         System.out.println("结果 = " + calculate(listString));
     }
 
+    /**
+     *  把后缀表达式转成list集合
+     * @param suffixExpression 后缀表达式
+     * @return 后缀表达式的list集合
+     */
     public static List<String> getListString(String suffixExpression) {
+        //根据空格进行分割
         String[] split = suffixExpression.split(" ");
         return new ArrayList<>(Arrays.asList(split));
     }
 
+    /**
+     * 计算
+     * @param ls 后缀表达式
+     * @return 最后计算的值
+     */
     public static int calculate(List<String> ls) {
         Stack<String> stack = new Stack<>();
+        //遍历后缀表达式
         for (String item : ls) {
+            //匹配数字
             if (item.matches("\\d+")) {
+                //如果是数字，将数字直接如栈
                 stack.push(item);
             } else {
+                //如果不是数字，判断运算符
                 int num1 = Integer.parseInt(stack.pop());
                 int num2 = Integer.parseInt(stack.pop());
-                int res = 0;
+                int res;
+                //根据操作符进行计算
                 switch (item) {
                     case "+":
                         res = num2 + num1;
@@ -62,37 +80,56 @@ public class PolandNotation {
         return Integer.parseInt(stack.pop());
     }
 
+    /**
+     * 中缀表达式转后缀表达式
+     * @param InfixExpression 前缀表达式
+     * @return 后缀表达式
+     */
     public static String toSuffixExpressionList(String InfixExpression) {
+        //符号栈
         Stack<String> s1 = new Stack<>();
-       List<String> s2 = new ArrayList<>();
+        //最终栈
+        List<String> s2 = new ArrayList<>();
+        // 把普通表达式转成list集合，并遍历
         for (String item : toInfixExpressionList(InfixExpression)) {
+            //匹配数字
             if (item.matches("\\d+")) {
+                //如果是数字直接加入s2栈中
                 s2.add(item);
             } else {
+                //如果不是，判断s1栈是否为空
                 if (s1.size() == 0) {
+                    //如是，则直接入栈
                     s1.push(item);
                 } else if (")".equals(item)) {
+                    //如不是，判断当前是否为右括号
                     while (!"(".equals(s1.peek())) {
+                        //如是，将弹出s1中的内容到s2中，直到碰到左括号
                         s2.add(s1.pop());
                     }
+                    //弹出左括号
                     s1.pop();
                 } else if ("(".equals(item)) {
+                    //如果是左括号直接入s1栈
                     s1.push(item);
                 } else if (getPriority(s1.peek()) < getPriority(item)) {
+                    //如果s1栈顶的优先级小于当前操作符的优先级，则入s1栈
                     s1.push(item);
                 } else {
+                    //否则弹出s1栈的符号入s2栈，并将当前的操作符入s1栈
                     s2.add(s1.pop());
                     s1.push(item);
                 }
             }
         }
+        //将s1栈中剩余的符号弹出入s2栈
         int size = s1.size();
         for (int i = 0; i < size; i++) {
             s2.add(s1.pop());
         }
 
+        //将s2中的内容输出
         StringBuilder sb = new StringBuilder();
-        size = s2.size();
         for (String s : s2) {
             sb.append(s);
             sb.append(" ");
@@ -100,17 +137,24 @@ public class PolandNotation {
         return sb.toString();
     }
 
+    /**
+     *  将普通表达式转中缀表达式的list集合
+     * @param s 普通表达式
+     * @return 中缀表达式的list集合
+     */
     private static ArrayList<String> toInfixExpressionList(String s) {
-//        Character.isDigit();
         ArrayList<String> list = new ArrayList<>();
         int i = 0;
         char c;
         StringBuilder str;
         do {
+            //判断当前字符是不是数字
             if (!Character.isDigit(c = s.charAt(i))) {
+                //如不是，则直接加入集合中
                 list.add("" + c);
                 i++;
             } else {
+                //如是，则把接下来是数字的字符加入String中
                 str = new StringBuilder();
                 while (i < s.length() && Character.isDigit(c = s.charAt(i))) {
                     str.append(c);
@@ -125,6 +169,11 @@ public class PolandNotation {
         return list;
     }
 
+    /**
+     * 获取操作符优先级
+     * @param s 操作符
+     * @return 优先级
+     */
     public static int getPriority(String s) {
         switch (s) {
             case "+":
